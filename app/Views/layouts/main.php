c63ba559c3159f76be5cc83ef7cd40163a3613da
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 <ardhisaddam@gmail.com>
2022-09-20 13:28:25 +0700

    update title

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= isset($title) ? $title : $title ; ?></title>
+    <title><?= isset($title) ? $title : "web programming 2" ; ?></title>
 </head>
 <body>
     <div>
