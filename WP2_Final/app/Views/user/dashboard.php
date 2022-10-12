4579f2b9441842e849a653943ee7314294017ae9
saddam-satria <ardhisaddam@gmail.com>
2022-10-12 11:25:58 +0700

    update detail btn

--- WP2_Final/app/Views/user/dashboard.php
+++ WP2_Final/app/Views/user/dashboard.php
@@ -23,11 +23,14 @@
 
     <section class="py-3 mt-5">
         <h5 style="color: #535353;">Cucian Anda Saat Ini</h5>
-        <div class="d-flex align-items-center text-white" style="border-radius: 20px; background-color: #21aee4;">
-            <div class="py-2 text-center" style="width: 50%; border-radius: 20px; background-color: #8dbafe;">
-                50%
+        <div class="d-flex">
+            <div class="d-flex align-items-center text-white" style="border-radius: 20px; background-color: #21aee4; flex:0.99">
+                <div class="py-2 text-center" style="width: 50%; border-radius: 20px; background-color: #8dbafe;">
+                    50%
+                </div>
+                <span class="ml-4">Progress</span>
             </div>
-            <span class="ml-4">Progress</span>
+            <a href="#" class="btn btn-md mx-2" style="background-color: #85f1fe; color: #000;">Detail</a>
         </div>
     </section>
 
