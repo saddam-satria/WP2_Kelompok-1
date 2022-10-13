dc78418eac71ce4d263fc8f61e0f72afc9da2a93
saddam-satria <ardhisaddam@gmail.com>
2022-10-13 07:32:16 +0700

    update order

--- WP2_Final/app/Views/user/order/index.php
+++ WP2_Final/app/Views/user/order/index.php
@@ -4,5 +4,22 @@
 
 <?= view("components/alert", array("key" => "error")) ?>
 
+<section class="py-3 mt-5">
+    <div class="d-flex mb-2">
+        <span style="color: #535353;">Service Name</span>
+        <div class="ml-auto">
+            <span style="color: #535353;">20 kg</span>
+        </div>
+    </div>
+    <div class="d-flex">
+        <div class="d-flex align-items-center text-white" style="border-radius: 20px; background-color: #21aee4; flex:0.99">
+            <div class="py-2 text-center" style="width: 50%; border-radius: 20px; background-color: #8dbafe;">
+                50%
+            </div>
+            <span class="ml-4">Progress</span>
+        </div>
+        <a href="#" class="btn btn-md mx-2" style="background-color: #85f1fe; color: #000;">Detail</a>
+    </div>
+</section>
 
 <?= $this->endSection(); ?>
