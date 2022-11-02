a555c069eb18d4b27fd40c528ca0cb24488fc657
saddam-satria <ardhisaddam@gmail.com>
2022-11-02 11:23:45 +0700

    update cart display

--- WP2_Final/app/Views/user/cart/index.php
+++ WP2_Final/app/Views/user/cart/index.php
@@ -1,3 +1,18 @@
+<?php
+$cartSummary = 0;
+$cart = $carts[0];
+$cartAsKG = 0;
+foreach ($carts as $cart) {
+    $qty = (float) $cart->quantity / $cart->quantityPerKG;
+    $cartAsKG = $cartAsKG + $qty;
+    $cartSummary = $cartSummary + $qty * $cart->itemPrice;
+}
+
+
+$cartSummary = $cartSummary + $cart->servicePrice + $cart->packagePrice;
+
+?>
+
 <?= $this->extend("layouts/user/dashboard"); ?>
 
 <?= $this->section("content"); ?>
@@ -11,18 +26,82 @@
 <?php if (!is_null($carts) && count($carts) > 0) : ?>
 
     <section class="py-3 mt-5">
-        <?php foreach ($carts as $cart) : ?>
+        <div class="row">
+            <div class="col-sm-12 col-md-8 mb-3">
+                <?php foreach ($carts as $cart) : ?>
+                    <div class="mb-5">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <img src="<?= base_url("assets/img/tshirt.png") ?>" alt="Pakaian">
+                            <div class="d-flex flex-column">
+                                <span style="font-weight: 600;">Jumlah</span>
+                                <span><?= $cart->quantity / $cart->quantityPerKG ?> kg</span>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <form action="#">
+                                    <button type="submit" class="btn btn-sm btn-primary">-</button>
+                                </form>
 
-            <span><?= $cart->serviceName ?></span>
+                                <span class="badge badge-sm badge-primary mx-1"><?= $cart->quantity ?> pcs</span>
 
-        <?php endforeach ?>
+                                <form action="#">
+                                    <button type="submit" class="btn btn-sm btn-primary">+</button>
+                                </form>
+                            </div>
+                            <div>
+                                <form action="#">
+                                    <button type="submit" class="btn btn-sm btn-danger">x</button>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="my-2 d-flex">
+                            <div>
+                                <span style="font-weight: 600;">Keterangan</span>
+                                <p class="text-capitalize"><?= $cart->description ?></p>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach ?>
 
 
-
+            </div>
+            <div class="col-sm-12 col-md-1">
+            </div>
+            <div class="col-sm-12 col-md-3">
+                <div class="d-flex flex-column" style="height: 100%; width: 100%;">
+                    <div class="d-flex justify-content-between mb-3">
+                        <span style="font-weight: 600;">Total Pakaian</span>
+                        <span><?= $cartAsKG ?> /kg</span>
+                    </div>
+                    <div class="d-flex justify-content-between mb-3">
+                        <span style="font-weight: 600;">Biaya service <?= $carts[0]->serviceName ?></span>
+                        <span><?= number_format($carts[0]->servicePrice) ?> /kg</span>
+                    </div>
+                    <div class="d-flex justify-content-between mb-3">
+                        <span style="font-weight: 600;">Biaya paket <?= $carts[0]->packageName ?></span>
+                        <span><?= number_format($carts[0]->packagePrice) ?> /kg</span>
+                    </div>
+                    <div class="d-flex justify-content-between mb-3">
+                        <span style="font-weight: 600;">Total Harga</span>
+                        <span><?= number_format($cartSummary) ?> /kg</span>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="d-flex my-5 justify-content-end">
             <a href="#" class="btn btn-md mx-2 btn-danger">reset</a>
             <a href="#" class="btn btn-md mx-2" style="background-color: #85f1fe; color: #000;">Checkout</a>
         </div>
+
+
+
+
+
+
+
+
+
+
+
     </section>
 <?php else : ?>
     <?= view("components/empty_data", array("message" => "keranjang anda masih kosong")) ?>
